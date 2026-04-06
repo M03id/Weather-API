d95b3147b94606d4492926602df3fd7e7c9b800c
mohamed eid <mo7amed22659@gmail.com>
2026-04-06 18:24:23 +0200

    integrate external weather API and update response format

--- api/views.py
+++ api/views.py
@@ -1,12 +1,35 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.conf import settings
+import requests
 
 @api_view(['GET'])
 def get_weather(request):
-    # Mock weather data
-    date = {
-        "location": "New York",
-        "temperature": "15°C",
-        "description": "Partly cloudy"
+    city = request.GET.get('city')
+    
+    if not city:
+        return Response({"error": "City parameter is required"}, status=400)
+    
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}"
+    params = {
+        "appid" : settings.WEATHER_API_KEY,
+        "units": "metric",
     }
-    return Response(date)
+    print(settings.WEATHER_API_KEY)
+    try:
+        response = requests.get(url, params=params)
+        print(response.status_code)
+        print(response.text)
+        print(response.json())
+        if response.status_code != 200:
+            return Response({"error": "Failed to fetch data"}, status=500)
+        
+        data = response.json()
+        result = {
+                "city": data["name"],
+                "temperature": data["main"]["temp"],
+                "condition": data["weather"][0]["description"]
+            }
+        return Response(result)
+    except Exception as e:
+        return Response({"error": str(e)}, status=500)
--- weather_api/settings.py
+++ weather_api/settings.py
@@ -117,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+from decouple import config
+WEATHER_API_KEY = config('WEATHER_API_KEY')
+REDIS_URL = config('REDIS_URL')
