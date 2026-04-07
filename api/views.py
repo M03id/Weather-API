8b77fb6d65e60dec073180e2dad57443dac950b1
mohamed eid <mo7amed22659@gmail.com>
2026-04-07 15:29:19 +0200

    implement Redis caching for weather API and add .env.example folder for the Environment Variables

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.conf import settings
+from django.core.cache import cache
 import requests
 
 @api_view(['GET'])
@@ -10,17 +11,23 @@ def get_weather(request):
     if not city:
         return Response({"error": "City parameter is required"}, status=400)
     
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}"
+    catched_data = cache.get(city)
+    if catched_data:
+        return Response({
+            "source": "cahce",
+            "data": catched_data
+        })
+
+    url = f"https://api.openweathermap.org/data/2.5/weather"
     params = {
+        "q": city,
         "appid" : settings.WEATHER_API_KEY,
         "units": "metric",
     }
-    print(settings.WEATHER_API_KEY)
+
     try:
         response = requests.get(url, params=params)
-        print(response.status_code)
-        print(response.text)
-        print(response.json())
+       
         if response.status_code != 200:
             return Response({"error": "Failed to fetch data"}, status=500)
         
@@ -30,6 +37,12 @@ def get_weather(request):
                 "temperature": data["main"]["temp"],
                 "condition": data["weather"][0]["description"]
             }
-        return Response(result)
+        
+        cache.set(city, result, timeout=60 * 60 * 12)  # Cache for 12 hours
+
+        return Response({
+            "source": "api",
+            "data": result  
+        })
     except Exception as e:
         return Response({"error": str(e)}, status=500)
